8959e851c3075be42b0721e810ddd4cc7696376f
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-02 10:27:46 -0600

    added findOneByUsername route at /useres/auth

--- 01-speedrun/src/users/users.controller.ts
+++ 01-speedrun/src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthUserDto } from './dto/auth-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -17,6 +26,10 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Post('/auth')
+  findOneByUsername(@Body() authUserDto: AuthUserDto) {
+    return this.usersService.findOneByUsername(authUserDto);
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
--- 01-speedrun/src/users/users.service.ts
+++ 01-speedrun/src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { AuthUserDto } from './dto/auth-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  private readonly users = [
+    { userId: 1, username: 'john', password: 'changeme' },
+    { userId: 2, username: 'maria', password: 'changeme' },
+  ];
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,6 +17,9 @@ export class UsersService {
     return `This action returns all users`;
   }
 
+  findOneByUsername(authUserDto: AuthUserDto) {
+    return authUserDto;
+  }
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
